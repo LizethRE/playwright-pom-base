557c71c078ac01b6dee9fd8fcdd3c07369f6a1a8
LizethRE <lizzriosepalza@gmail.com>
2024-08-12 08:57:45 -0500

    Se agregan escenarios alternos para el inicio de sesión, pruebas basadas en data driven usando archivos JSON.

--- pages-objects/LogInPage.ts
+++ pages-objects/LogInPage.ts
@@ -1,16 +1,22 @@
-import {Locator, Page} from "@playwright/test";
+import {expect, Locator, Page} from "@playwright/test";
 
 export class LogInPage {
     private readonly page: Page;
+    private readonly logInPageTitleLabel: Locator;
     private readonly usernameInput: Locator;
     private readonly passwordInput: Locator;
     private readonly loginButton: Locator;
+    private readonly alertLabel: Locator;
+    private readonly requiredFieldAlertLabel: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.logInPageTitleLabel = page.getByRole('heading', {name: 'Login'});
         this.usernameInput = page.getByPlaceholder('Username');
         this.passwordInput = page.getByPlaceholder('Password');
         this.loginButton = page.getByRole('button', {name: 'Login'});
+        this.alertLabel = page.getByText('Invalid credentials');
+        this.requiredFieldAlertLabel = page.getByText('Required');
     }
 
     async goTo() {
@@ -30,8 +36,28 @@ export class LogInPage {
     }
 
     async loginWithCredentials(username: string, password: string) {
+        if (username.localeCompare("N/a", 'es', {sensitivity: 'base'}) === 0) {
+            username = "";
+        }
+        if (password.localeCompare("N/a", 'es', {sensitivity: 'base'}) === 0) {
+            password = "";
+        }
+
         await this.fillUsername(username);
         await this.fillPassword(password);
         await this.clickOnLogin();
+    }
+
+    async checkFailedLoginAlert(message: string) {
+        await expect(this.alertLabel).toHaveText(message);
+    }
+
+    async checkLogInPageVisibility() {
+        await this.page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        await expect(this.logInPageTitleLabel).toBeVisible();
+    }
+
+    async checkRequiredFieldVisibility() {
+        await expect(this.requiredFieldAlertLabel.nth(0) || this.requiredFieldAlertLabel.nth(1)).toBeVisible();
     }
 }
--- tests/log-in.spec.ts
+++ tests/log-in.spec.ts
@@ -2,20 +2,51 @@ import {test} from '@playwright/test';
 import {LogInPage} from '../pages-objects/LogInPage';
 import {DashboardPage} from '../pages-objects/DashboardPage';
 
+import user from '../test-data/login.json';
+import incorrectUsers from '../test-data/incorrect_users.json';
+import incompleteUsers from '../test-data/incomplete_users.json';
+
 let logInPage: LogInPage;
 
 test.describe('Inicio de Sesión en OrangeHRM', () => {
-    test('Inicio de sesión exitoso', async ({page}) => {
+    test.beforeEach(async ({page}) => {
         logInPage = new LogInPage(page);
         await test.step('Navega a la pagina Inicio de Sesión', async () => {
             await logInPage.goTo();
         })
+    });
+
+    test('Inicio de sesión exitoso', async ({page}) => {
         await test.step('Se autentica en el sistema', async () => {
-            await logInPage.loginWithCredentials('Admin', 'admin123');
+            await logInPage.loginWithCredentials(user.username, user.password);
         })
         await test.step('Válida la autenticación exitosa', async () => {
-            const dashboardPage = new DashboardPage(page)
+            const dashboardPage = new DashboardPage(page);
             await dashboardPage.checkDashboardPageVisibility();
         })
     });
+
+    incorrectUsers.forEach(user => {
+        test(`Inicio de sesión fallido con credenciales incorrectas ${user.key}`, async () => {
+            await test.step('Se autentica en el sistema', async () => {
+                await logInPage.loginWithCredentials(user.username, user.password);
+            })
+            await test.step('Válida la alerta de Invalid credentials y la permanencia en la página de Inicio de Sesión', async () => {
+                await logInPage.checkFailedLoginAlert('Invalid credentials');
+                await logInPage.checkLogInPageVisibility();
+            })
+        });
+    })
+
+    incompleteUsers.forEach(user => {
+        test(`Inicio de sesión fallido con campos vacíos ${user.key}`, async () => {
+            await test.step('Se autentica en el sistema', async () => {
+                await logInPage.loginWithCredentials(user.username, user.password);
+            })
+            await test.step('Válida la alerta de Required y la permanencia en la página de Inicio de Sesión', async () => {
+                await logInPage.checkRequiredFieldVisibility();
+                await logInPage.checkLogInPageVisibility();
+            })
+        });
+    })
 });
